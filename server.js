1fb4a4ccab5183d7987864b4c22f7df9f2612d91
myrriame <myrriametiffany@gmail.com>
2021-04-27 22:27:52 -0400

    first 2

--- server.js
+++ server.js
@@ -7,6 +7,44 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 
+// Create a GET endpoint that returns all Users
+app.get('/users', async (req, res) => {
+  const users = await db.any("SELECT * FROM users").then((users) => {
+    return users;
+  })
+
+
+  res.send(users);
+})
+
+
+// Create a GET endpoint that returns all Comments
+app.get('/comments', async (req, res) => {
+  const comments = await db.any("SELECT * FROM comments").then((comments) => {
+    return comments;
+  })
+
+
+
+  res.send(comments);
+})
+
+
+// Create a POST endpoint that allows you to create a User
+
+
+
+// Create a POST endpoint that allows you to create a Comment 
+
+
+// Create a PUT endpoint that allows you to update a User 
+
+
+// Create a PUT endpoint that allows you to update a Comment
+
+
+// Create a GET endpoint that allows you to retrieve a User's Comments
+
 
 
 app.listen(PORT, () => {
