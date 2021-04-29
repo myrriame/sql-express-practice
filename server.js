58fa181a157db1b27ab439fa219c40db7f804530
myrriame <myrriametiffany@gmail.com>
2021-04-28 21:18:27 -0400

    update

--- server.js
+++ server.js
@@ -7,8 +7,20 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 
+// Create a full set of CRUD endpoints to:
+// Create a specific item in the food_des table
+// Read a specific item from the food_des table
+// Update a specific item in the food_des table
+
+// Delete a specific item in the food_des table
+
+// Create an endpoint to fetch all items in the fd_group table
+
+// Create an endpoint to fetch a single item in the fd_group given an fdgrp_cd
+
+// Modify the endpoint in #3 to return a list of foods that belong to the food group
 
 
 app.listen(PORT, () => {
-  console.log(`LikeyPix API is running on port ${PORT}`);
+  console.log(`Food API is running on port ${PORT}`);
 });
