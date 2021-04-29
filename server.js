fa80d13d84a414dec12d24d699f072b9c20fdb7d
myrriame <myrriametiffany@gmail.com>
2021-04-28 21:22:05 -0400

    update

--- server.js
+++ server.js
@@ -7,8 +7,17 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 
+// Create an endpoint to retrieve all Classes
+// Create an endpoint to retrieve all Students
+// Create an endpoint to retrieve all Teachers
+// Create an endpoint to retrieve all Students given a specific Class ID
+// Create an endpoint to retrieve all Classes given a specific Teacher ID
+// Create an endpoint to retrieve a single Class given a specific ID
+// Create an endpoint to retrieve a single Student given a specific ID
+// Bonus:
+// Modify your endpoint in Question 6 by also adding a list of Students that belong to the class in a students key
 
 
 app.listen(PORT, () => {
-  console.log(`LikeyPix API is running on port ${PORT}`);
+  console.log(`Classroom API is running on port ${PORT}`);
 });
