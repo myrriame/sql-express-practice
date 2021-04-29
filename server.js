eb53c42b67f636ec896729c84bbf557e99d73160
myrriame <myrriametiffany@gmail.com>
2021-04-28 21:15:42 -0400

    update

--- server.js
+++ server.js
@@ -34,17 +34,36 @@ app.get('/comments', async (req, res) => {
 
 
 
+
 // Create a POST endpoint that allows you to create a Comment 
 
 
 // Create a PUT endpoint that allows you to update a User 
+app.put('/users/:id', async (req, res) => {
+  const id = req.params.id;
+  const name = req.params.name;
+  const email = req.params.email;
+  await db.none("UPDATE users SET name = $1, email = $2 WHERE id = $3", [name, email, id]).then((users) => {
 
+
+
+    res.send(`${name} has been updated`);
+  });
+});
 
 // Create a PUT endpoint that allows you to update a Comment
 
 
 // Create a GET endpoint that allows you to retrieve a User's Comments
+app.get('/users/:comments', async (req, res) => {
+  const comments = await db.any(`SELECT * FROM comments WHERE users = ${req.params.users}`).then(() => {
+    return comments;
+  })
 
+
+
+  res.send(comments);
+})
 
 
 app.listen(PORT, () => {
