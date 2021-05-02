8dfd71466e5b54e3137991b7c561b3f9f7759a95
myrriame <myrriametiffany@gmail.com>
2021-05-01 21:41:50 -0400

    first 3

--- server.js
+++ server.js
@@ -8,12 +8,42 @@ const PORT = 3000;
 app.use(bodyParser.json());
 
 // Create an endpoint to retrieve all Classes
+app.get('/classes', async (req, res) => {
+  const classes = await db.any("SELECT * FROM classes").then((classes) => {
+    return classes;
+  })
+
+
+  res.send(classes);
+})
 // Create an endpoint to retrieve all Students
+app.get('/students', async (req, res) => {
+  const students = await db.any("SELECT * FROM students").then((students) => {
+    return students;
+  })
+
+
+  res.send(students);
+})
 // Create an endpoint to retrieve all Teachers
+app.get('/teachers', async (req, res) => {
+  const teachers = await db.any("SELECT * FROM teachers").then((teachers) => {
+    return teachers;
+  })
+
+
+  res.send(teachers);
+})
 // Create an endpoint to retrieve all Students given a specific Class ID
+
 // Create an endpoint to retrieve all Classes given a specific Teacher ID
+
 // Create an endpoint to retrieve a single Class given a specific ID
+
+
 // Create an endpoint to retrieve a single Student given a specific ID
+
+
 // Bonus:
 // Modify your endpoint in Question 6 by also adding a list of Students that belong to the class in a students key
 
